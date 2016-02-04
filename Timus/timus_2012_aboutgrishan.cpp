eeff4573b66f4202d7f5c368235c657157facde3
Baichuan Yang <1062969706@qq.com>
2016-02-04 13:32:54 -0800

    Create timus_2012_aboutgrishan.cpp

---
+++ Timus/timus_2012_aboutgrishan.cpp
@@ -0,0 +1,38 @@
+/*
+*
+* Tag: Implementation
+* Time: O(1)
+* Space: O(1)
+*/
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <bitset>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+//#include <unordered_map>
+using namespace std;
+const int N = 90;
+const int M = 205;
+const int MOD = 10037;
+const double eps = 1e-6;
+int f;
+
+int main(){
+//    freopen("fence4.in", "r", stdin);
+//    freopen("fence4.out", "w", stdout);
+    scanf("%d",&f);
+    if (f >= 7) {
+        puts("YES");
+    } else {
+        puts("NO");
+    }
+    return 0;
+}

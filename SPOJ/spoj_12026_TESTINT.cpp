b0f943033de8dba86f017bc32e02a0b1da98be4a
Baichuan Yang <1062969706@qq.com>
2016-02-16 20:22:12 -0800

    Create spoj_12026_TESTINT.cpp

---
+++ SPOJ/spoj_12026_TESTINT.cpp
@@ -0,0 +1,30 @@
+/*
+*
+* Tag: Math
+* Time: O(1)
+* Space: O(1)
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 1010;
+const int M = 101010;
+const double eps = 1e-10;
+int a, b;
+
+int main() {
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a+b);
+    return 0;
+}

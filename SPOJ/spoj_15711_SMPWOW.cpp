2b29841301e02bc04fd8278e8d37de45b944b2c9
Baichuan Yang <1062969706@qq.com>
2016-02-18 22:17:12 -0800

    Create spoj_15711_SMPWOW.cpp

---
+++ SPOJ/spoj_15711_SMPWOW.cpp
@@ -0,0 +1,34 @@
+/*
+*
+* Tag: Implementation
+* Time: O(n)
+* Space: O(1)
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 110;
+const int M = 101010;
+const double eps = 1e-10;
+int n;
+
+int main() {
+    scanf("%d",&n);
+    printf("W");
+    for (int i = 0; i < n; ++ i) {
+        printf("o");
+    }
+    puts("w");
+    return 0;
+}

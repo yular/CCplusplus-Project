b817846a39f85606c1f9d7925f7ba33a55153949
Baichuan Yang <1062969706@qq.com>
2016-02-19 19:52:26 -0800

    Create spoj_17103_SMPSEQ4.cpp

---
+++ SPOJ/spoj_17103_SMPSEQ4.cpp
@@ -0,0 +1,51 @@
+/*
+ *
+ * Tag: Implementation
+ * Time: O(n)
+ * Space: O(n)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 310;
+const int M = 101010;
+const double eps = 1e-10;
+int n, m;
+int s[N];
+unordered_set<int> q;
+
+int main() {
+    scanf("%d",&n);
+    for (int i = 0; i < n; ++ i) {
+        scanf("%d",&s[i]);
+    }
+    scanf("%d",&m);
+    for (int i = 0; i < m; ++ i) {
+        int v;
+        scanf("%d",&v);
+        q.insert(v);
+    }
+    bool isfirst = true;
+    for (int i = 0; i < n; ++ i) {
+        if (q.find(s[i]) != q.end()) {
+            if (!isfirst) {
+                printf(" ");
+            }
+            printf("%d", s[i]);
+            isfirst = false;
+        }
+    }
+    puts("");
+    return 0;
+}

0e0a8515a583940e22b66726378ea5e67f2bc6f0
Baichuan Yang <1062969706@qq.com>
2016-02-21 00:35:07 -0800

    Create spoj_17994_PCROSS1.cpp

---
+++ SPOJ/spoj_17994_PCROSS1.cpp
@@ -0,0 +1,64 @@
+/*
+ *
+ * Tag: Implementation
+ * Time: O(nm)
+ * Space: O(nm)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 310;
+const int M = 101010;
+const double eps = 1e-10;
+int n, m, x, y;
+char mp[N][N];
+
+void init(){
+    memset(mp, 0, sizeof(mp));
+    for (int i = 0; i < n; ++ i) {
+        for (int j = 0; j < m; ++ j) {
+            mp[i][j] = '.';
+        }
+    }
+}
+
+int main() {
+    int T;
+    scanf("%d",&T);
+    while (T --) {
+        scanf("%d%d%d%d",&n,&m,&x,&y);
+        init();
+        -- x;
+        -- y;
+        for (int i = x; i >= 0; -- i) {
+            mp[i][y] = '*';
+        }
+        for (int i = x; i < n; ++ i) {
+            mp[i][y] = '*';
+        }
+        for (int j = y; j >= 0; -- j) {
+            mp[x][j] = '*';
+        }
+        for (int j = y; j < m; ++ j) {
+            mp[x][j] = '*';
+        }
+        for (int i = 0; i < n; ++ i) {
+            puts(mp[i]);
+        }
+        if (T) {
+            puts("");
+        }
+    }
+    return 0;
+}

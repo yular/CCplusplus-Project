ef0d5050bceaa2d70a0e8d72e885fb09cf41e68b
Baichuan Yang <1062969706@qq.com>
2016-02-26 20:26:11 -0800

    Create spoj_17659_ABCLOG.cpp

---
+++ SPOJ/spoj_17659_ABCLOG.cpp
@@ -0,0 +1,45 @@
+/*
+ *
+ * Tag: Bit Manipulation
+ * Time: O(lgn)
+ * Space: O(1)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 500;
+const int M = 20;
+const double eps = 1e-10;
+long long n;
+
+int main() {
+    while (scanf("%lld",&n) == 1) {
+        if (n < 0) {
+            break;
+        }
+        long long ans = 0;
+        while (n) {
+            if (n&1) {
+                ans |= 1;
+            }
+            n >>= 1;
+            if (!n) {
+                break;
+            }
+            ans <<= 1;
+        }
+        printf("%lld\n",ans);
+    }
+    return 0;
+}

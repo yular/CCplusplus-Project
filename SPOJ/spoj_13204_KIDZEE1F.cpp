b9cd403e1838045072fae6a8dce98956df80c03c
Baichuan Yang <1062969706@qq.com>
2016-03-03 21:05:06 -0800

    Create spoj_13204_KIDZEE1F.cpp

---
+++ SPOJ/spoj_13204_KIDZEE1F.cpp
@@ -0,0 +1,31 @@
+/*
+ *
+ * Tag: Implementation
+ * Time: O(1)
+ * Space: O(1)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 100020;
+const int M = 1030000;
+const int MOD = 100;
+const double eps = 1e-10;
+
+int main() {
+    for (int i = 1000; i > 0; -- i) {
+        printf("%d\n",i);
+    }
+    return 0;
+}

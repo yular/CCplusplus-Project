1bf1a017fa58dac71de85faf85098d9528d428a7
Baichuan Yang <1062969706@qq.com>
2016-03-04 19:35:26 -0800

    Create spoj_13206_KIDZEE1G.cpp

---
+++ SPOJ/spoj_13206_KIDZEE1G.cpp
@@ -0,0 +1,39 @@
+/*
+ *
+ * Tag: Math
+ * Time: O(n/x)
+ * Space: O(1)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 3000200;
+const int M = 1030000;
+const int MOD = 100;
+const double eps = 1e-10;
+int x, n;
+
+int main() {
+    int T, cas = 1;
+    scanf("%d",&T);
+    while (T --) {
+        scanf("%d%d",&x,&n);
+        printf("Case %d:", cas++);
+        for (int i = 1; i*x <= n; ++ i) {
+            printf(" %d",i*x);
+        }
+        puts("");
+    }
+    return 0;
+}

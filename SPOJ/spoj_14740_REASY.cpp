2ba2e3e2e978ee44dda25d93ce235d036c2082d3
Baichuan Yang <1062969706@qq.com>
2016-03-06 20:00:02 -0800

    Create spoj_14740_REASY.cpp

---
+++ SPOJ/spoj_14740_REASY.cpp
@@ -0,0 +1,49 @@
+/*
+ *
+ * Tag: Implementation
+ * Time: O(n)
+ * Space: O(1)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 1300;
+const int M = 300;
+const int MOD = 100;
+const double eps = 1e-10;
+int n;
+
+int main() {
+    int T, cas = 1;
+    scanf("%d",&T);
+    while (T --) {
+        scanf("%d",&n);
+        int ans = 0;
+        for (int i = 0; i < n; ++ i) {
+            int v;
+            scanf("%d",&v);
+            if (!i) {
+                ans += v;
+            } else {
+                if (v >= 0) {
+                    ans += v;
+                } else {
+                    ans -= v;
+                }
+            }
+        }
+        printf("Case %d: %d\n",cas++,ans);
+    }
+    return 0;
+}

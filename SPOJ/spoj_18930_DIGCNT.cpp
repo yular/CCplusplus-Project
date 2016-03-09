c82e7d8f903ccc4a1c67613577cf6aa3fae6f565
Baichuan Yang <1062969706@qq.com>
2016-03-08 20:54:15 -0800

    Create spoj_18930_DIGCNT.cpp

---
+++ SPOJ/spoj_18930_DIGCNT.cpp
@@ -0,0 +1,36 @@
+/*
+ *
+ * Tag: Implementation
+ * Time: O(n)
+ * Space: O(n)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 100010;
+const int M = 30;
+const long long MOD = 1000000007;
+const double eps = 1e-10;
+char s[N];
+
+
+int main() {
+    int T;
+    scanf("%d",&T);
+    while (T --) {
+        scanf("%s",&s);
+        printf("%d\n",strlen(s));
+    }
+    return 0;
+}

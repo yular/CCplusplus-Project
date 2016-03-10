2b1076196e4ff2a646e2cc119c6170ea9cf283d2
Baichuan Yang <1062969706@qq.com>
2016-03-09 23:05:09 -0800

    Create spoj_21722_FUACM001.cpp

---
+++ SPOJ/spoj_21722_FUACM001.cpp
@@ -0,0 +1,31 @@
+/*
+ *
+ * Tag: Implementation
+ * Time: O(n)
+ * Space: O(n)
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+const int N = 1000200;
+const int M = 1030;
+const long long MOD = 1000000007;
+const double eps = 1e-10;
+long long a, b;
+
+int main() {
+    scanf("%lld%lld",&a,&b);
+    printf("%lld\n%lld\n%lld\n",a+b,a-b,a*b);
+    return 0;
+}

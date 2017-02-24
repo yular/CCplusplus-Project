bbc8bc76dd2bfacf16068a65ec4e4fa4cce14b29
Baichuan Yang <1062969706@qq.com>
2017-02-24 00:59:39 -0800

    Create spoj_25177_VARIATIONSTM.cpp

---
+++ SPOJ/spoj_25177_VARIATIONSTM.cpp
@@ -0,0 +1,52 @@
+/*
+*
+* Tag: Math
+* Time: O(n)
+* Space: O(1)
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+using namespace std;
+const int N = 2000200;
+const int M = 300;
+const long long MOD = 1000000007;
+const double PI = acos(-1.0);
+const double eps = 1e-10;
+const int MAX_VAL = 800*1000;
+int ans;
+char s[M];
+
+int main(){
+    int T;
+    scanf("%d",&T);
+    while (T --) {
+        scanf("%s",s);
+        ans = 1;
+        for (int i = 0; s[i]; ++ i) {
+            if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' ||
+                s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' ||
+                s[i] == 's' || s[i] == 'S') {
+                ans *= 3;
+            } else {
+                ans <<= 1;
+            }
+        }
+        printf("%d\n",ans);
+    }
+    return 0;
+}

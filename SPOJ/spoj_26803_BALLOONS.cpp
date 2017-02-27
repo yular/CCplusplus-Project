229107096ce593fd56860d1e75d61a11be666bd1
Baichuan Yang <1062969706@qq.com>
2017-02-27 00:07:23 -0800

    Create spoj_26803_BALLOONS.cpp

---
+++ SPOJ/spoj_26803_BALLOONS.cpp
@@ -0,0 +1,15 @@
+/*
+*
+* Tag: Math
+* Time: O(1)
+* Space: O(1)
+*/
+#include <iostream>
+using namespace std;
+int n, p, c;
+
+int main(){
+    scanf("%d%d%d",&n,&p,&c);
+    printf("%d\n",n*p*c);
+    return 0;
+}

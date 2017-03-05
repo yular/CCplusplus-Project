c48e94b261632c32f39973df2943ade451f4176d
Baichuan Yang <1062969706@qq.com>
2017-03-05 00:21:34 -0800

    Create spoj_24911_VOCALES.cpp

---
+++ SPOJ/spoj_24911_VOCALES.cpp
@@ -0,0 +1,47 @@
+/*
+*
+* Tag: Implementation
+* Time: O(n)
+* Space: O(1)
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+using namespace std;
+const int N = 10002;
+const int M = 300;
+const long long MOD = 495;
+const double PI = acos(-1.0);
+const double eps = 1e-10;
+const int MAX_VAL = 800*1000;
+char t[N];
+string s;
+
+int main(){
+    getline(cin, s);
+    int len_t = 0;
+    memset(t, 0, sizeof(t));
+    for (int i = 0; i < s.length(); ++ i) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            t[len_t ++] = (s[i] - 32);
+        } else if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+            t[len_t ++] = s[i];
+        }
+    }
+    puts(t);
+    return 0;
+}

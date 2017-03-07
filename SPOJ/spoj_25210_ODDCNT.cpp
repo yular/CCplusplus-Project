9af3500361e354b69b7f62c24817aea57041d1aa
Baichuan Yang <1062969706@qq.com>
2017-03-07 01:42:00 -0800

    Create spoj_25210_ODDCNT.cpp

---
+++ SPOJ/spoj_25210_ODDCNT.cpp
@@ -0,0 +1,51 @@
+/*
+*
+* Tag: Math
+* Time: O(1)
+* Space: O(1)
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+using namespace std;
+const int N = 210002;
+const int M = 300;
+const long long MOD = 495;
+const double PI = acos(-1.0);
+const double eps = 1e-10;
+const int MAX_VAL = 800*1000;
+int u, v, ans;
+
+int main(){
+    int T;
+    scanf("%d",&T);
+    while (T --) {
+        scanf("%d%d",&u,&v);
+        ans = 0;
+        if (u%2 == 0) {
+            ++ u;
+        }
+        if (v%2 == 0) {
+            -- v;
+        }
+        if (u <= v) {
+            ans = (v - u)/2 + 1;
+        }
+        printf("%d\n",ans);
+    }
+    return 0;
+}

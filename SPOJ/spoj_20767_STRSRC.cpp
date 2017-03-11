a57a6ddfa7b721de07648232b110e1cc49494cdc
Baichuan Yang <1062969706@qq.com>
2017-03-11 11:59:04 -0800

    Create spoj_20767_STRSRC.cpp

---
+++ SPOJ/spoj_20767_STRSRC.cpp
@@ -0,0 +1,45 @@
+/*
+*
+* Tag: String
+* Time: O(n)
+* Space: O(n)
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+using namespace std;
+const int N = 210002;
+const int M = 300;
+const long long MOD = 495;
+const double PI = acos(-1.0);
+const double eps = 1e-10;
+const int MAX_VAL = 800*1000;
+string s, p;
+
+int main(){
+    int T;
+    scanf("%d",&T);
+    while (T --) {
+        cin>>s>>p;
+        if (s.find(p) != -1) {
+            puts("Yes");
+        } else {
+            puts("No");
+        }
+    }
+    return 0;
+}

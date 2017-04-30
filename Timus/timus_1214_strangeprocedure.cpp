e71a6cef9c4802b62b917ab547b89391cbd1ade6
Baichuan Yang <1062969706@qq.com>
2017-04-30 00:48:09 -0700

    Create timus_1214_strangeprocedure.cpp

---
+++ Timus/timus_1214_strangeprocedure.cpp
@@ -0,0 +1,40 @@
+/*
+*
+* Tag: Implementation
+* Time: O(1)
+* Space: O(1)
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+using namespace std;
+const int N = 200;
+const int M = 10300;
+const long long MOD = 495;
+const double PI = acos(-1.0);
+const double DOUBLEMAX = 10000000001;
+const double eps = 1e-10;
+const int MAX_VAL = 800*1000;
+long long x, y;
+
+int main(){
+    scanf("%lld%lld",&x,&y);
+    if (!((x<0)||(y<0)||((x+y)%2==0)))
+        swap(x, y);
+    printf("%lld %lld\n",x,y);
+    return 0;
+}

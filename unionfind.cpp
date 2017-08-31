c06cd174147d0328c52c87ae74532d6f702e8d7e
Baichuan Yang <1062969706@qq.com>
2017-08-30 19:53:57 -0700

    Update unionfind.cpp

--- unionfind.cpp
+++ unionfind.cpp
@@ -10,8 +10,8 @@ public:
   
   int find( int u ){
     int s = u;
-    for(s = u; fa[s] >= 0; s = fa[s]) ;
-    while(s != u){
+    for( s = u; fa[s] >= 0; s = fa[s] ) ;
+    while( s != u ){
       int tmp = fa[u];
       fa[u] = s;
       u = tmp;
@@ -22,7 +22,7 @@ public:
   int unionset( int a, int b ){
     int fa1 = find(a), fa2 = find(b);
     int tmp = fa[fa1]+fa[fa2];
-    if(fa[fa1] > fa[fa2]){
+    if( fa[fa1] > fa[fa2] ){
       fa[fa1] = fa2;
       fa[fa2] = tmp;
     }else{

877de71b658287b4f5ebe93a45e24d4ef3f7b8e4
Baichuan Yang <1062969706@qq.com>
2017-10-17 23:06:40 -0700

    Update floyd.cpp

--- floyd.cpp
+++ floyd.cpp
@@ -25,9 +25,9 @@ public:
   
   void floyd(){
     
-    for(int k = 0; k < n; ++ k){
-      for(int i = 0; i < n; ++ i){
-        for(int j = 0; j < n; ++ j){
+    for( int k = 0; k < n; ++ k ){
+      for( int i = 0; i < n; ++ i ){
+        for( int j = 0; j < n; ++ j ){
           graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
         }
       }

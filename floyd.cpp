64e90501085dae2ef12e4b9dc88858004b1bb8fa
Baichuan Yang <1062969706@qq.com>
2017-11-21 23:29:13 -0800

    Update floyd.cpp

--- floyd.cpp
+++ floyd.cpp
@@ -24,7 +24,6 @@ public:
   }
   
   void floyd(){
-    
     for( int k = 0; k < n; ++ k ) {
       for( int i = 0; i < n; ++ i ) {
         for( int j = 0; j < n; ++ j ) {
@@ -32,7 +31,6 @@ public:
         }
       }
     }
-    
   }
   
   int getDist( int s, int t ){
